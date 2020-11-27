e3cef444e46171a9253f5d763726b119ef2ddbce
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-11-27 11:13:59 -0300

    Atualização do exercício 9

--- exercises/bloco_4/4.2/exercicio9.js
+++ exercises/bloco_4/4.2/exercicio9.js
@@ -1,14 +1,13 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
 let arrayAteVinteCinco = []
-let arrayAteVinteCincoPares = []
+let arrayAteVinteCincoPor2 = []
 
 for (let index = 1; index <= 25; index +=1) {
   arrayAteVinteCinco.push(index)
 } console.log(arrayAteVinteCinco.join(", "))
 
 for (let index = 0; index < arrayAteVinteCinco.length; index +=1) {
-  if (arrayAteVinteCinco[index] % 2 == 0) {
-    arrayAteVinteCincoPares.push(arrayAteVinteCinco[index])
-  }
-} console.log(arrayAteVinteCincoPares)
+    arrayAteVinteCincoPor2.push(arrayAteVinteCinco[index] / 2)
+
+} console.log(arrayAteVinteCincoPor2)
